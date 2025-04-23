cbe18b0f65186670f433a091bcaa377597fd3956
Heyguimendes <guimendes2000@hotmail.com>
2025-04-23 02:01:20 -0300

    Correções

--- servico/rota_material.js
+++ servico/rota_material.js
@@ -8,21 +8,21 @@ function exibirMaterial(req, res) {
 
 // Função para cadastrar o material no banco de dados
 function cadastrarMaterial(req, res) {
-    const { descricao, valor_kg, peso, linha } = req.body;
+    const { descricao, valor_kg, linha } = req.body;
 
     // Verifica se todos os campos obrigatórios foram preenchidos
-    if (!descricao || !valor_kg || !peso || !linha) {
+    if (!descricao || !valor_kg || !linha) {
         return res.status(400).send('Preencha todos os campos obrigatórios!');
     }
 
     const conexao = conectiondb();
 
     const query = `
-    INSERT INTO materiais (ds_material, vl_valor_por_kg, qt_peso_kg, ie_linha)
-    VALUES (?, ?, ?, ?)
+    INSERT INTO materiais (ds_material, vl_valor_por_kg, ie_linha)
+    VALUES (?, ?, ?)
     `;
 
-    conexao.query(query, [descricao, valor_kg, peso, linha], (err, resultado) => {
+    conexao.query(query, [descricao, valor_kg, linha], (err, resultado) => {
         if (err) {
             console.error('Erro ao cadastrar material:', err);
             return res.status(500).send('Erro no servidor ao cadastrar o material.');
@@ -33,7 +33,6 @@ function cadastrarMaterial(req, res) {
         console.log('Dados registrados:', {
             descricao,
             valor_kg,
-            peso,
             linha
         });
 
@@ -41,7 +40,6 @@ function cadastrarMaterial(req, res) {
             material: {
                 descricao,
             valor_kg,
-            peso,
             linha
             },
             script: `
