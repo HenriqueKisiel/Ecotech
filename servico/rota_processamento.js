392fb0e3f040273d824fd87f7aeb6849b3c50b50
AlceuMM <alceu_maia@hotmail.com>
2025-06-17 22:17:18 -0300

    Ajuste

--- servico/rota_processamento.js
+++ servico/rota_processamento.js
@@ -2,7 +2,9 @@ const conectiondb = require('../bd/conexao_mysql.js');
 
 // Função para exibir processamento
 function exibirProcessamento(req, res) {
-    res.render('processamento');
+    res.render('processamento', { 
+        usuario: req.session.usuario
+    });
 }
 //exportando a função 
 module.exports = {
